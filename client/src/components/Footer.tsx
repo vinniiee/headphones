87e905873c1baad29680bfdfb232931cff2801e1
vinniiee <vs28245@gmail.com>
2024-06-22 19:32:24 +0530

    fix: footer art margin

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,14 +7,18 @@ const Footer = () => {
       <Container>
         <Row>
           <Col>
-            <div className="flex justify-center  items-center gap-4 relative top-[300px] py-2 z-10">
-              <Logo color="white" className="w-48 sm:w-auto"/> <p className="mt-2">&copy; {new Date().getFullYear()}</p>
+            <div className="flex justify-center  items-center gap-4 relative top-[325px] sm:top-[300px] py-2 z-10">
+              <Logo color="white" className="w-48 sm:w-auto" />{" "}
+              <p className="mt-2">&copy; {new Date().getFullYear()}</p>
             </div>
           </Col>
         </Row>
       </Container>
-      <Image src="/footer.png"  alt="footer-art" className="w-full absolute  -bottom-[300px] z-0" />
-
+      <Image
+        src="/footer.png"
+        alt="footer-art"
+        className="w-full absolute -bottom-0 md:-bottom-[100px]  lg:-bottom-[200px] xl:-bottom-[300px] z-0"
+      />
     </footer>
   );
 };
